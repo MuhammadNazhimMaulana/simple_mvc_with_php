3217baab07e13c44570ad5fa9fa35ec790ad31cb
MuhammadNazhimMaulana <nazhimmaulanamuhammad@gmail.com>
2022-06-25 18:47:29 +0700

    Finishing Routing

--- app/core/App.php
+++ app/core/App.php
@@ -4,6 +4,24 @@ class App {
 
     public function __construct()
     {
-        echo 'Jalan';
+       $url = $this->parseUrl();
+       var_dump(($url));
+    }
+
+    // Parsing URL
+    public function parseUrl()
+    {
+        if( isset($_GET['url']) ){
+
+            // Cleaning url that is got using rtrim
+            $url = rtrim($_GET['url'], '/');
+
+            // Cleaning Url from any dangerous character
+            $url = filter_var($url, FILTER_SANITIZE_URL);
+
+            // Dividing url
+            $url = explode('/', $url);
+            return $url;
+        }
     }
 }
